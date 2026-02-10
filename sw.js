822f75ac779efffc3f9b96507fb59c11d97d3b5f
Qoo-tw <323328tht@gmail.com>
2026-02-10 16:54:16 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "card-helper-v4";
+const CACHE = "card-helper-v5";
 const ASSETS = [
   "./",
   "./index.html",
@@ -18,6 +18,7 @@ self.addEventListener("fetch", (e) => {
     caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
+
 
 
 
